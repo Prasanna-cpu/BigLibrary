4e1f0bba4a540b62e1d98e19b7e9abbb74866272
Prasanna-cpu <kumar22maran@gmail.com>
2026-09-01 20:04:28 +0530

    Add log

--- catalog-service/src/main/java/com/biglibrary/catalog_service/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/biglibrary/catalog_service/CatalogServiceApplication.java
@@ -1,5 +1,6 @@
 package com.biglibrary.catalog_service;
 
+import org.hibernate.internal.log.SubSystemLogging;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
 		System.setProperty("user.timezone", "Asia/Kolkata");
+		System.out.println("Welcome");
 		SpringApplication.run(CatalogServiceApplication.class, args);
 	}
 
