46a51adfc63231dc9dd86d0fe4f18b5ab8a1a8bf
Prasanna-cpu <kumar22maran@gmail.com>
2026-09-01 20:08:44 +0530

    Added Spotless Apply to CI/CD workflow , along with Welcome Log

--- catalog-service/src/main/java/com/biglibrary/catalog_service/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/biglibrary/catalog_service/CatalogServiceApplication.java
@@ -1,6 +1,5 @@
 package com.biglibrary.catalog_service;
 
-import org.hibernate.internal.log.SubSystemLogging;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +8,7 @@ public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
 		System.setProperty("user.timezone", "Asia/Kolkata");
-		System.out.println("Welcome");
+		System.out.println("Welcome to Catalog Service");
 		SpringApplication.run(CatalogServiceApplication.class, args);
 	}
 
