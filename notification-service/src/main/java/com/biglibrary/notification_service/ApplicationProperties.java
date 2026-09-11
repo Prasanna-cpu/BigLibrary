33a9779fcc8cf58badef23a236de4f3ed4fffc09
Prasanna-cpu <kumar22maran@gmail.com>
2026-09-11 18:53:04 +0530

    Commit 2

--- notification-service/src/main/java/com/biglibrary/notification_service/ApplicationProperties.java
+++ notification-service/src/main/java/com/biglibrary/notification_service/ApplicationProperties.java
@@ -1,4 +1,9 @@
 package com.biglibrary.notification_service;
 
-public class ApplicationProperties {
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "notifications")
+public record ApplicationProperties(String orderEventsExchange, String newOrdersQueue,
+                                    String deliveredOrdersQueue, String cancelledOrdersQueue, String errorOrdersQueue) {
 }
--- notification-service/src/main/java/com/biglibrary/notification_service/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/biglibrary/notification_service/NotificationServiceApplication.java
@@ -1,18 +1,18 @@
-package com.biglibrary.order_service;
+package com.biglibrary.notification_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@SpringBootApplication
 @ConfigurationPropertiesScan
-@SpringBootApplication(scanBasePackages = "com.biglibrary")
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImplementation")
-public class OrderServiceApplication {
+public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Now running Order Service");
-		SpringApplication.run(OrderServiceApplication.class, args);
+		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 
 }
